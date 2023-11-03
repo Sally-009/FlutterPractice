7bac2be5445e79b210052d97f1b8a44b032addc7
Saori Kojima <skojima@atu.edu>
2023-11-02 19:36:18 -0500

    changed "login.dart" to "login_screen.dart"

--- practice_app/lib/login_screen.dart
+++ practice_app/lib/login_screen.dart
@@ -28,6 +28,7 @@ class _LoginState extends State<Login> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            // UserName
             TextField(
               onChanged: (value) {
                 setState(() {
@@ -36,6 +37,7 @@ class _LoginState extends State<Login> {
               },
               decoration: const InputDecoration(labelText: 'User Name'),
             ),
+            // Password
             TextField(
               onChanged: (value) {
                 setState(() {
@@ -46,9 +48,21 @@ class _LoginState extends State<Login> {
               obscureText: true, // パスワードを隠す
             ),
             const SizedBox(height: 20),
+            // Submit
+            ElevatedButton(
+              // login関数の中身を書けば赤線解決すると思う
+              onPressed: login(_uname, _psw),
+              child: const Text("Done"),
+            ),
           ],
         ),
       ),
     );
+  }
+
+  // ログイン画面でDoneを押した時の処理(フォームで入力されたunameとpswが引数)
+  void login(String unamein, String pswin) {
+    // ユーザーデータと照合
+    // 有効な結果なら次のページへ行く
   }
 }
