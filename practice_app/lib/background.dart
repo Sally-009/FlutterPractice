91a8b942a509fa8c4dc04698d73329402edf13d3
Saori Kojima <skojima@atu.edu>
2023-11-02 19:54:04 -0500

    必要最低限のコード追加

--- practice_app/lib/background.dart
+++ practice_app/lib/background.dart
@@ -1,2 +1,21 @@
 // This is used for the background design and page transition
 import 'package:flutter/material.dart';
+
+class Background extends StatefulWidget {
+  const Background({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _BackgroundState();
+  }
+}
+
+class _BackgroundState extends State<Background> {
+  @override
+  Widget build(context) {
+    // ページ遷移（表示するコンテンツ）
+
+    //背景デザインはココ
+    return const MaterialApp();
+  }
+}
